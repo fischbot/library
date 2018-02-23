9d60be6127e381fd2b8ceb1cf63931cf74409cf5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-02-22 21:49:15 -0500

    change sample data

--- js/main.js
+++ js/main.js
@@ -552,22 +552,13 @@ function populateSampleData() {
                       'Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum.',
                       '', '', setId(), false
   );
-  const twoTowers = new Book('The Two Towers', 'J.R.R. Tolkien', '',
-                            'Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum.',
-                            '', '', setId(), false);
-  const rotc = new Book('The Return of the King', 'J.R.R. Tolkien', '', '', '', '', setId(), false);
-
-  const prideAndPrejudice = new Book('Pride and Prejudice', 'Jane Austin', 1870, '', 'https://books.google.com/books/content?id=dalDAAAAcAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 332, setId(), false);
-
-  const animalFarm = new Book('Animal Farm', 'George Orwell', '', 'George Orwell\’s famous satire of the Soviet Union, in which “all animals are equal but some animals are more equal than others.”', 'https://books.google.com/books/content?id=nkalO3OsoeMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 144, setId(), true);
 
   const nineteen84 = new Book('1984', 'George Orwell', 2008, '', '',  325, setId(), true);
 
+  const got = new Book('A Game of Thrones 4-Book Bundle', '2011-03-22', 'George R. R. Martin', 'George R. R. Martin\'s A Song of Ice and Fire series has become, in many ways, the gold standard for modern epic fantasy. Martin—dubbed the "American Tolkien" by Time magazine—has created a world that is as rich and vital as any piece of historical fiction, set in an age of knights and chivalry and filled with a plethora of fascinating, multidimensional characters that you love, hate to love, or love to hate as they struggle for control of a divided kingdom. It is this very vitality that has led it to be adapted as the HBO miniseries “Game of Thrones.” This bundle includes the following novels: A GAME OF THRONES A CLASH OF KINGS A STORM OF SWORDS A FEAST FOR CROWS', 'https://books.google.com/books/content?id=mA8A4BYWB1IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 3264, setId(), false);
+
   myLibrary.push(hobbit);
   myLibrary.push(fotr);
-  myLibrary.push(twoTowers);
-  myLibrary.push(rotc);
-  myLibrary.push(prideAndPrejudice);
-  myLibrary.push(animalFarm);
   myLibrary.push(nineteen84);
+  myLibrary.push(got);
 }
