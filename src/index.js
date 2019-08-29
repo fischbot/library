d5383b83853c26a11e6322d9c9604f9da4e95231
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-08-29 15:34:41 -0400

    add handleClick event listener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Library from './Library/Library';
 import View from './View/View';
+import handleClick from './utils/handleClick';
 import './main.css';
 
 (function() {
   const library = new Library();
   const view = new View();
+  document.addEventListener(
+    'click',
+    e => handleClick(e, library.books, view.updateReadView),
+    false
+  );
   view.renderLibrary(library.books);
 })();
