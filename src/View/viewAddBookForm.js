0df00c01b1f475f2492009bdfda8ec4b416c9f51
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-09-02 17:53:27 -0400

    move toggleModalView to viewHelpers.js

--- src/View/viewAddBookForm.js
+++ src/View/viewAddBookForm.js
@@ -1,15 +1,10 @@
+import { toggleModalView } from '../utils/viewHelpers';
+
 function viewAddBookForm(id) {
   const formToView = id.includes('custom')
     ? 'custom-book-modal'
     : 'search-modal';
-  const modal = document.getElementById(formToView);
-  toggleModalView(modal);
-}
-
-function toggleModalView(modal) {
-  modal.classList.contains('hidden')
-    ? modal.classList.remove('hidden')
-    : modal.classList.add('hidden');
+  toggleModalView(formToView);
 }
 
 export default viewAddBookForm;
