4ab9b725eae923b9adecf9ef824fd0ea815ae5da
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-09-10 16:22:45 -0400

    delete media queries scss and put code in main.scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import UserLibrary from './UserLibrary/UserLibrary';
-import ViewUserLibrary from './ViewUserLibrary/ViewUserLibrary';
 import handleClick from './utils/handleClick';
 import sampleData from './assets/sample_data';
 import './main.scss';
-import './mediaQueries.scss';
 
 (function() {
   const library = new UserLibrary(sampleData);
